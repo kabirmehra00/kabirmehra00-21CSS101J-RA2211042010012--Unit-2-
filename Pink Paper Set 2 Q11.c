992fb18ef1464f8eb69fbd82e90a1d0473575ef2
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-21 21:32:44 +0530

    Create Pink Paper Set 2 Q11.c

---
+++ Pink Paper Set 2 Q11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+  printf("RA2211042010012\n");
+  int number;
+  int N,M;
+  printf("Enter the dimensions of box\n");
+  scanf("%d%d",&N,&M);
+  number=N*M;
+  printf("Number of containers required=%d",number);
+}
