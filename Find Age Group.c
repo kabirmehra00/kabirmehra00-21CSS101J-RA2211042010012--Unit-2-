328ceca8674fd53e1213589397b8dfe04c435e39
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-21 23:53:57 +0530

    Update Find Age Group.c

--- Find Age Group.c
+++ Find Age Group.c
@@ -4,11 +4,11 @@ void main() {
 	int age, infant_age = 3, child_age = 13, senior_citizen_age = 60, centenarian_age = 100;
 	printf("Enter age : ");
 	scanf("%d", &age);
-	if (age<=infant_age || age>=centenarian_age  ) { // if condition
+	if (age<=infant_age || age>=centenarian_age  ) {
 		printf("Ticket Price: 0\n");
-	} else if (age<=child_age || age>=senior_citizen_age ) { // else if condition
+	} else if (age<=child_age || age>=senior_citizen_age ) {
 		printf("Ticket Price: 5\n");
-	} else { // else
+	} else {
 		printf("Ticket Price: 10\n");
 	}
 }
