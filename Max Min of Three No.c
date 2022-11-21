9ec579aaf132a32c9c89778ffaacc80ea654f1f1
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-21 12:37:44 +0530

    Create Max Min of Three No.c

---
+++ Max Min of Three No.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main(){
+	int a, b, c;
+	printf("Enter 3 numbers : ");
+	scanf("%d %d %d",&a,&b,&c);
+	if (a>b){
+		if (b>c){
+			printf("The max and min of three numbers are : %d %d\n", a, c);
+		} else if(a>c){
+			printf("The max and min of three numbers are : %d %d\n", a ,b);
+		} else {
+			printf("The max and min of three numbers are : %d %d\n", c, b);
+		}
+	}
+	else {
+		if (a>c){
+			printf("The max and min of three numbers are : %d %d\n", b, c);
+		} else if(c>b){
+			printf("The max and min of three numbers are : %d %d\n", c, a);
+		} else {
+			printf("The max and min of three numbers are : %d %d\n", b, a);
+		}
+	}
+}
