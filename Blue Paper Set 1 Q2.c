73fab473101fafb651465ea04111ad7d8b25f38a
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-21 21:13:24 +0530

    Create Blue Paper Set 1 Q2.c

---
+++ Blue Paper Set 1 Q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  printf("RA2211042010012\n");
+  int X,Y;
+  scanf("%d %d", &X, &Y);
+  if(Y<=X)
+  {
+    printf("%d\n", Y);
+  }else{
+    printf("%d\n", X);
+  }
+}
