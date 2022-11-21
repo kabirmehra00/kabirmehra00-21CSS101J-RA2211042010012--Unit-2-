a092b2d25a26cdbc1e2bbbc2c154edf9e4bed987
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-21 23:27:12 +0530

    Create Nested loop.c

---
+++ Nested loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+  printf("RA2211042010012\n");
+	int i, j;
+	for (i = 1; i <= 3; i++) {
+		for (j = 1; j <= 3; j++) {
+			if (i == j) {
+				break;
+			}
+			printf("i : %d, j : %d\n", i, j);
+			}
+		}
+}
