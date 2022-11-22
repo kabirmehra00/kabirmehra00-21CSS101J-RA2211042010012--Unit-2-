47071960bbe7cc79807678ad89e3b691e0c84c19
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 09:02:28 +0530

    Create change value with if.c

---
+++ change value with if.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010012\n");
+	int a = 500, b = 100, c;
+	if (!a >= 400)
+		b = 300;
+	c = 200;
+	printf("b = %d c = %d\n", b, c);
+	return 0;
+}
