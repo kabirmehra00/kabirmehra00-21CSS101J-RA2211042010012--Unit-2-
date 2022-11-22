351bbb0e5da766d7cfb7922f64b2cd2d3998fea4
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 13:27:43 +0530

    Create Q10.c

---
+++ Q10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("RA2211042010012")
+    int no_of_rows;
+    scanf("%d",&no_of_rows);
+    for(int i=1;i<=no_of_rows;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ", i);
+        }
+    printf("\n");
+    }
+}
