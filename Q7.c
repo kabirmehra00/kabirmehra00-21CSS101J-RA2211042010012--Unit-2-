910a08f14c569989c949ba1f9e514817ddfd6b2f
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 12:43:51 +0530

    Update Q7.c

--- Q7.c
+++ Q7.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
   int n,r;
-    scanf("%d %d",&n,&r);
-    if(n>=10) {
-      printf("%d",r);
-    } else{
+  printf("RA2211042010012\n");
+  scanf("%d %d",&n,&r);
+  if(n>=10) {
+    printf("%d",r);
+  } else{
     printf("%d",r+(100*(10-n)));
-    }
+  }
 }
