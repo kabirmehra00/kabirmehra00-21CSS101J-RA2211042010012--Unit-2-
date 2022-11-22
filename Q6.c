072fc6ad3045cef1f6bd905356772468cfe3fc29
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 12:43:17 +0530

    Update Q6.c

--- Q6.c
+++ Q6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){   
+    printf("RA2211042010012\n");
     int length,width,height,surfacearea;
     scanf("%d%d%d%d",&length,&width,&height,&surfacearea);
     surfacearea=2*(width*length+length*height+height*width);
