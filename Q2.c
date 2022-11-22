a97b24dc525f8003463f85033960bdb6707fcd4f
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 12:42:01 +0530

    Update and rename Q2 to Q2.c

--- Q2.c
+++ Q2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+    printf("RA2211042010012\n");
     float radiusofball,volumeofball;
     scanf("%f",&radiusofball);
     volumeofball=(4/3)*3.14*pow(radiusofball,3);
