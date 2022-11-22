0472cd2369283f0b4d7d4aed762f477d43c76dcc
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 12:37:26 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){   
+    int length,width,height,surfacearea;
+    scanf("%d%d%d%d",&length,&width,&height,&surfacearea);
+    surfacearea=2*(width*length+length*height+height*width);
+    printf("%d\n",surfacearea);
+}
