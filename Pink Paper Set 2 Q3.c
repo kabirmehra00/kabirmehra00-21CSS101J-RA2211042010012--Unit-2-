d423f39e8e136076cb9545cded5302ef18e19d3e
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 09:47:13 +0530

    Create Pink Paper Set 2 Q3.c

---
+++ Pink Paper Set 2 Q3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void asc_sort(int a[100],int n)
+{
+    int i,j,temp;
+     for(i=0; i<n-1; i++)
+    {
+        for(j=0; j<n-i-1; j++){
+           if(a[j]>a[j+1]){           {
+               temp=a[j];
+               a[j]=a[j+1];
+               a[j+1]=temp;
+                                 }
+        }
+    }
+}
+int main()
+{
+    int a[100],i,n;
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+  asc_sort(a,n);
+    for(i=0; i<n; i++)
+    {
+       printf("%d ",a[i]);
+    }
+    return 0;
+ }
