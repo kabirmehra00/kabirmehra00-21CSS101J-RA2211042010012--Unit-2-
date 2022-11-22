159232b3bde940a1cf9b160e6227f673566526e3
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-22 12:46:13 +0530

    Update Q9.c

--- Q9.c
+++ Q9.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+    printf("RA2211042010012\n");
     int apple1,apple2,apple3;
     scanf("%d%d%d",&apple1,&apple2,&apple3);
     if((apple2>apple1) && (apple3>apple2)){
