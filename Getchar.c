17e528c321d6c6f90c2c6e29992a18e10422f4b4
kabirmehra00 <113982798+kabirmehra00@users.noreply.github.com>
2022-11-28 19:44:15 +0530

    Create Getchar.c

---
+++ Getchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>    
+int main()  
+{  
+    char c;   
+    printf ("\n Enter a character \n");  
+    c = getchar(); 
+    printf(" You have passed ");  
+    putchar(c); //
+    getch();  
+}
